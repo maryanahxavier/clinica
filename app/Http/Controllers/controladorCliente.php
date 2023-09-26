36525ae6037da666a9ad27f8303265eb4ea6e85f
maryanahvaxier <maryanahxavi9@gmail.com>
2023-09-26 15:07:25 -0300

    controller1

--- app/Http/Controllers/controladorCliente.php
+++ app/Http/Controllers/controladorCliente.php
@@ -18,15 +18,17 @@ class controladorCliente extends Controller
     }
     public function index()
     {
-        $
+        $cliente = new CLiente();
+        return view('sistema.cliente', compact('cliente'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
     public function create()
+    
     {
-        //
+        return view('sistema.novoCliente')
     }
 
     /**
@@ -34,7 +36,11 @@ class controladorCliente extends Controller
      */
     public function store(Request $request)
     {
-        //
+       $dados = new Cliente();
+       $dados->descricaoCliente = ('descricaoCliente');
+       $dados->save();
+       return redirect('/cliente') ->with('sucess', 'Novo cliente cadastrado com secesso');
+
     }
 
     /**
@@ -50,7 +56,12 @@ class controladorCliente extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $dados = Cliente::find($id);
+        if(isset($cliente)){
+            return view('sistema.editarCliente', compact('dados'));
+    
+        }
+        return redirect('/cliente') ->with('danger', 'cliente não encontrado')
     }
 
     /**
@@ -58,7 +69,15 @@ class controladorCliente extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $cliente = Cliente::find($id);
+        if(isset('$dados')){
+            $dados->descricaoCliente = $request->input('descricaoCliente');
+            $dados-save();
+        
+     } else{
+        return redirect('/cliente')->with('danger', "erro ao tentar atualizar o cadastro de cliente");
+     }
+     return redirect('/cliente')->with('sucess', 'Cadastro atualizado com sucesso');
     }
 
     /**
@@ -66,6 +85,29 @@ class controladorCliente extends Controller
      */
     public function destroy(string $id)
     {
-        //
+         $dados = Cliente::find($id);
+         if(isset($dados)){
+            $consulta = Cliente::where('cliente_id','=' , $id)->first[];
+            if(!isset('consultas')){
+                $dados->delete();
+            }else{
+                return reirect('/clientes') ->with('danger' , 'Erro ao tentar excluir o cadastro.')
+            }
+         }else{
+            return redirect('/clientes')->with('danger', 'Cliente não encontrado');
+         }
+         return redirect('clientes') ->('success', 'Cadastro exluído com sucesso');
+
+    }
+
+    public function pesquisarCliente()
+    {
+        return view('sistema.pequisarCliente');
+    }
+
+    public function procurarClinte(Request $request)
+    {
+       ///terminar
+
     }
 }
